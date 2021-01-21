7043852253c8d370a8df8a6db6715aabb68cbd56
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-20 23:03:04 -0800

    Can add name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
-  const [newName, setNewName] = useState('');
+  const [newName, setNewName] = useState('Add name here');
+
+  const handleChange = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  // let res;
+  // useEffect(() => {
+  //   res = persons.map((person, index) => {
+  //     return <p key={index}>{person.name}</p>;
+  //   });
+  // }, [persons]);
+
+  const handleSubmit = event => {
+    // debugger;
+    setPersons([...persons, { name: newName }]);
+    event.preventDefault();
+    // console.log(event.target.value);
+    // setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
-        return <p>{person.name}</p>;
+      {/* {JSON.stringify(persons)} */}
+      {persons.map((person, index) => {
+        return <p key={index}>Name: {person.name}</p>;
       })}
+      {/* res: {res} */}
     </div>
   );
 };
